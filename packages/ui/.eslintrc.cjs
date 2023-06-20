9ade9f326b0f9a73568696c12d56113ffba70f4e
collins401 <xiefeitai@gmail.com>
2023-06-20 23:27:34 +0800

    feat: add eslint test rules

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ['custom'],
+  rules: {
+    "@typescript-eslint/no-redeclare": "off",
+  }
 }
