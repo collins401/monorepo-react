c0c482089c03c46216454ba9e5ad816d230cff4e
collins401 <373345619@qq.com>
2023-06-27 19:14:22 +0800

    feat: add nprogress

--- packages/ui/src/Loading/index.tsx
+++ packages/ui/src/Loading/index.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react'
 import { Spin } from 'antd'
+import { useEffect } from 'react'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 export function Loading() {
+  useEffect(() => {
+    nprogress.start()
+    return () => {
+      nprogress.done()
+    }
+  })
   return (
     <div className="text-center p-4">
       <Spin />
